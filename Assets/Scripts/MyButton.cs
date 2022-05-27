459899b5c268b40529fb393b066c0bcd2699bfe6
Astrydax <astrydax@gmail.com>
2022-05-27 08:06:54 -0400

    Created button logic
    modified button script and clicksates to reset lastclicked to null on loop.

--- Assets/Scripts/MyButton.cs
+++ Assets/Scripts/MyButton.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,34 +7,51 @@ public class MyButton : ClickableObject
 {
     public GameObject unpressed, pressed;
 
-    public bool testFlag;
+    public bool isPressed = false;
 
     public int clickers = 0;
     private void Start()
     {
         unpressed.SetActive(true);
         pressed.SetActive(false);
+        GameEventsManager.instance.onPlayerRespawn += OnPlayerRespawn;
+    }
 
+    private void OnDestroy()
+    {
+        // unsubscribe from events
+        GameEventsManager.instance.onPlayerRespawn -= OnPlayerRespawn;
+    }
+
+    private void OnPlayerRespawn()
+    {
+        clickers = 0;        
     }
 
     public override void OnClick(GameObject clicker)
     {
-        clickers++;
-        pressed.SetActive(true);
-        unpressed.SetActive(false);       
+        clickers++;       
 
     }
 
     public override void OnRelease(GameObject clicker)
     {
         clickers--;
-        //ONLY CALL THIS IF NO CURSORS ARE CLICKING
-        if(clickers <= 0)
+    }
+
+    private void Update()
+    {
+        if(clickers > 0)
         {
+            isPressed = true;
+            pressed.SetActive(true);
+            unpressed.SetActive(false);
+        }
+        else
+        {
+            isPressed = false;
             pressed.SetActive(false);
             unpressed.SetActive(true);
-        }   
-        
-        
+        }
     }
 }
